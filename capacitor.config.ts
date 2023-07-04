f48d3a4614f10e1c692a67d1979bf09c1a3150ec
RyotaFukumoto <ryota.fukumoto@nextbeat.net>
2023-07-04 22:30:23 +0900

    First Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'position_setting_sample',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+<div class="container">
+    <div class="item">アイテム1</div>
+    <div class="item">アイテム2</div>
+    <div class="item">アイテム3</div>
+    <div class="item">アイテム4</div>
+</div>
+</main>
+
+<style>
+.container {
+    display: flex;
+}
+
+.item {
+    margin: 5px;
+    border: solid 1px #00ff7f;
+    background-color: #87c3fa;
+}
+</style>
